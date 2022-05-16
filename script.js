60cea075dc0fed6c791cf5ec068867481cfd2727
rinxkizrin <101653427+rinxkizrin@users.noreply.github.com>
2022-05-16 20:42:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,18 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     var result = document.getElementById("result").value;
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-  
-    
-    
+ 
 }
+
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
+    
+}
+//validate the principal. If negative or 0 alert user and focus on principal
+if(principal <= 0){
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
     
 }
